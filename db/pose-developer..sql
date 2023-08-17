210b04022f12270fc1392d2ebb27b79b8a8cb0b5
wangzijian <1219116968@qq.com>
2023-08-17 11:06:15 +0800

    update: update pose-developer.sql #2

--- db/pose-developer..sql
+++ db/pose-developer..sql
@@ -1,8 +1,102 @@
-create table if not exists  pose-developer.user
-(
-    id   int          null,
-    name varchar(255) null
-    );
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: pose-developer
+-- ------------------------------------------------------
+-- Server version	8.0.32
 
-insert into pose-developer.user (id, name)
-values (1,"wangzijian");
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `algorithm`
+--
+
+DROP TABLE IF EXISTS `algorithm`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `algorithm` (
+                             `id` int NOT NULL AUTO_INCREMENT,
+                             `name` varchar(255) DEFAULT NULL,
+                             `type` int DEFAULT NULL COMMENT '类型(0普通算法 1专用算法  2普通数据集 3 专用数据集)',
+                             `sport_category` int DEFAULT NULL,
+                             `template_id` int DEFAULT NULL,
+                             `install_type` int DEFAULT NULL COMMENT '部署方式（0：云端部署 1：本地部署）',
+                             `uploader` int DEFAULT NULL,
+                             `file` varchar(255) DEFAULT NULL,
+                             `docs` text,
+                             `example` text,
+                             `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+                             PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='算法表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `algorithm`
+--
+
+INSERT INTO `algorithm` VALUES (1,'足球算法1',0,1,1,1,1,'ea','文档','这是一个案例','2023-08-05 23:04:09'),(2,'足球算法2',0,1,1,1,1,'ea','文档','这是一个案例','2023-08-05 23:04:09'),(3,'足球算法2',1,1,1,1,1,'ea','文档','这是一个案例','2023-08-06 15:43:11'),(5,'王子健算法',0,1,1,1,1,'ea','文档','这是一个案例','2023-08-06 15:43:11');
+
+--
+-- Table structure for table `data_set`
+--
+
+DROP TABLE IF EXISTS `data_set`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `data_set` (
+                            `id` int NOT NULL AUTO_INCREMENT,
+                            `name` varchar(255) DEFAULT NULL COMMENT '数据集名',
+                            `type` int DEFAULT NULL COMMENT '数据集类型（0:普通数据集 ,1:专用数据集）',
+                            `sport_category` int DEFAULT NULL COMMENT '支持的体育类型',
+                            `file` varchar(255) DEFAULT NULL COMMENT '文件',
+                            `demo` text COMMENT '数据集样例',
+                            `install_type` int DEFAULT NULL COMMENT '部署方式（0：云端部署 1：本地部署）',
+                            `uploader` int DEFAULT NULL COMMENT '上传人',
+                            `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+                            PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='数据集表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `data_set`
+--
+
+INSERT INTO `data_set` VALUES (1,'足球数据集',1,1,'4b4901b8-7af4-444a-b044-3067902cfaf2','demo',1,1,'2023-08-08 23:57:51');
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+                        `id` int DEFAULT NULL,
+                        `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` VALUES (1,'wangzijian');
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKsS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-08-17 11:05:37
