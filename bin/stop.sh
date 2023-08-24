252fc89e5fe5352021fed44e72ab51a2be9aa701
wangzijian <1219116968@qq.com>
2023-08-24 23:47:05 +0800

    update: update bin/stop.sh  #6

--- bin/stop.sh
+++ bin/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PID=$(ps -ef | grep 'com.zlht.pbr.algorithm.management.AlgorithmDeveloperApi' | grep -v grep | awk '{print $2}')
+PID=$(ps -ef | grep 'com.zlht.pbr.algorithm.developer.AlgorithmDeveloperApi' | grep -v grep | awk '{print $2}')
 
 if [ -z "$PID" ]; then
     echo "No AlgorithmDeveloperApi process found to stop"
@@ -8,18 +8,6 @@ if [ -z "$PID" ]; then
 fi
 
 echo "Stopping AlgorithmDeveloperApi process $PID"
-kill -SIGTERM $PID
+kill $PID
 
-# Wait for up to 30 seconds for the process to stop
-for i in {1..30}; do
-    if ps -p $PID > /dev/null; then
-        sleep 1
-    else
-        echo "AlgorithmDeveloperApi process $PID stopped"
-        exit 0
-    fi
-done
-
-echo "AlgorithmDeveloperApi process $PID did not stop in a timely manner, sending SIGKILL"
-kill -SIGKILL $PID
 exit 0
